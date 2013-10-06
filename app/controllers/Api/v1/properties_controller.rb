efe45f14a2df1b96d7ccd9454de33ee27ab74c10
Stephen Fiser <stephen.fiser@gmail.com>
2013-10-06 11:16:13 -0500

    got json api working

--- app/controllers/Api/v1/properties_controller.rb
+++ app/controllers/Api/v1/properties_controller.rb
@@ -1,8 +1,31 @@
 class API::V1::PropertiesController < ApplicationController
+	respond_to :json
+
 	def index
-		@properties = Property.all
-		 respond_to do |format|
-	      format.json { render json: @properties }
-	    end
+		respond_with Property.all
+	end
+
+	def show
+		respond_with Property.find(params[:id])
+	end
+
+	def create 
+		property = Property.create(property_params)
+		respond_with :api, :v1, property
+	end
+
+	def update
+		respond_with Property.update(params[:id], params[:property])
+	end
+
+	def destroy
+		respond_with Property.destroy(params[:id])
+	end
+
+
+	private 
+
+	def property_params
+		params.require(:property).permit(:title)
 	end
 end
